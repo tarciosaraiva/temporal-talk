c8716a761237394e759c55a2318566d32e5a48a2
Tarcio Saraiva <tsaraiva@block.xyz>
2025-06-23 08:04:33 -0700

    transform into child workflow

--- workflow.go
+++ workflow.go
@@ -4,11 +4,30 @@ import (
 	"fmt"
 	"time"
 
+	"go.temporal.io/api/enums/v1"
 	"go.temporal.io/sdk/temporal"
 	"go.temporal.io/sdk/workflow"
 )
 
-func MainWorkflow(ctx workflow.Context, input string) (*WeatherOutput, error) {
+func MainWorkflow(ctx workflow.Context, input string) (WeatherOutput, error) {
+	// This is the main workflow that orchestrates the child workflow.
+	// It will call the ChildActionWorkflow and handle its result.
+	cwo := workflow.ChildWorkflowOptions{
+		ParentClosePolicy: enums.PARENT_CLOSE_POLICY_REQUEST_CANCEL,
+	}
+	ctx = workflow.WithChildOptions(ctx, cwo)
+
+	var result WeatherOutput
+
+	err := workflow.ExecuteChildWorkflow(ctx, ChildActionWorkflow, input).Get(ctx, &result)
+	if err != nil {
+		return result, fmt.Errorf("Failed to run child workflow: %s", err)
+	}
+
+	return result, nil
+}
+
+func ChildActionWorkflow(ctx workflow.Context, input string) (WeatherOutput, error) {
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: time.Minute, // maximum time the activity can run
 		RetryPolicy: &temporal.RetryPolicy{
@@ -27,26 +46,26 @@ func MainWorkflow(ctx workflow.Context, input string) (*WeatherOutput, error) {
 
 	err := workflow.ExecuteActivity(ctx, basicActivity.RunBasicActivity, input).Get(ctx, &result.Name)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to run basic activity: %s", err)
+		return result, fmt.Errorf("Failed to run basic activity: %s", err)
 	}
 
 	err = workflow.ExecuteActivity(ctx, remoteServiceActivity.GetIP).Get(ctx, &result.IpAddress)
 	if err != nil {
-		return nil, fmt.Errorf("Could not get IP: %s", err)
+		return result, fmt.Errorf("Could not get IP: %s", err)
 	}
 
 	var geoPoint Geopoint
 	err = workflow.ExecuteActivity(ctx, remoteServiceActivity.GetLocationInfo, result.IpAddress).Get(ctx, &geoPoint)
 	if err != nil {
-		return nil, fmt.Errorf("Could not geolocate with IP: %s", err)
+		return result, fmt.Errorf("Could not geolocate with IP: %s", err)
 	}
 
 	result.City = geoPoint.City
 
 	err = workflow.ExecuteActivity(ctx, remoteServiceActivity.GetWeather, geoPoint.Latitude, geoPoint.Longitude).Get(ctx, &result.CurrentForecast)
 	if err != nil {
-		return nil, fmt.Errorf("Could not retrieve weather: %s", err)
+		return result, fmt.Errorf("Could not retrieve weather: %s", err)
 	}
 
-	return &result, nil
+	return result, nil
 }
